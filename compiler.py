6ec18d45c5a6b8a46415708451f7c619047fa374
Ivanne <hed-icmanaois@smu.edu.ph>
2026-05-15 09:52:30 +0800

    Tokenize pa lang

--- compiler.py
+++ compiler.py
@@ -1,23 +1,53 @@
-Compiler for our Little Language (.PIC)
+'''
+Compiler for our Language (.PIC)
+'''
 
 import sys
-
+ 
 # read arguments
 program_filepath = sys.argv[1]
 
 print("[CMD] Parsing")
-
 #########################
 #  Tokenize Program
 #########################
 
 # read file lines
 program_lines = []
-with open(program_filepath, "r") as f:
+with open(program_filepath, "r") as program_file:
     program_lines = [
         line.strip()
             for line in program_file.readlines()]
 
 program = []
 for line in program_lines:
-    if    
+    parts = line.split(" ")
+    opcode = parts[0]
+
+    # check if for empty opcode
+    if opcode == "":
+        continue
+
+    #store opcode token
+    program.append(opcode)
+
+    #HANDLE EACH OPCODE
+    if opcode == "PUSH":
+        # expecting a number
+        number = int(parts[1])
+        program.append(number)
+    elif opcode == "PRINT":
+        # parse string literal
+        string_literal = ' '.join(parts[1:])[1:-1]
+        program.append(string_literal)
+    elif opcode == "JUMP.EQ.0":
+        # read label
+        label = parts[1]
+        program.append(label)
+    elif opcode == "JUMP.GT.0":
+        # read label
+        label = parts[1]
+        program.append(label)
+
+#read program
+print(program)
